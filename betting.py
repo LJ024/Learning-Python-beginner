5e6a983dc674d1035747d6052b6ff02d9a80d5b9
lesley <lesleyjoseph024@gmail.com>
2020-04-25 08:00:04 -0400

    betting game v1.1 Added if logic that tells you if you win or lose.

--- betting.py
+++ betting.py
@@ -2,5 +2,9 @@ import random
 
 # generate a random number
 win_number = random.randint (1, 3)
-guess = input("Guess the winning number.")
-print(win_number)
+guess = input("Guess the winning number:")
+print(f"The lucky number is {win_number}")
+if int(guess) == win_number:
+    print("Wow! Such a genius!")
+else:
+    print("You suck at this!")
