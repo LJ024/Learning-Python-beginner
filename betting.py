892e5ba3a3f726330ec3bfebec222ee4ab0fa0d3
lesley <lesleyjoseph024@gmail.com>
2020-04-25 18:29:34 -0400

    betting game v1.4 Added a scoring to the game and made it more readable for the user by putting in print() at various points in the code. Remember that wins=losses=o means that both commands has the same functions.

--- betting.py
+++ betting.py
@@ -1,14 +1,23 @@
 import random
 
+#score tracking variables
+wins = losses = 0
 play_again = "y"
 while play_again == "y":
     # generate a random number
     win_number = random.randint(1, 3)
-    guess = int(input("Guess the winning number:"))
+    guess = int(input("Chose a number between 1 and 3 to play:"))
+    print()
     print(f"The lucky number is {win_number}")
     if int(guess) == win_number:
+        wins += 1
         print("Wow! Such a genius!")
     else:
+        losses += 1
         print("You suck at this!")
+        print()
+    print(f"Score:wins{wins}, losses{losses}")
+    print()
     play_again = input("Do you dare to play again? (y/n)")
+    print()
 
