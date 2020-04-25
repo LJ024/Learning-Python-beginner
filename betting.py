f6a20fd56c526a9628f8dfd6f4f6477e4b0e95a5
lesley <lesleyjoseph024@gmail.com>
2020-04-25 17:32:57 -0400

    betting game v1.3 Adding While loop successfully

--- betting.py
+++ betting.py
@@ -1,10 +1,14 @@
 import random
-while play_again == "y"
-# generate a random number
-win_number = random.randint (1, 3)
-guess = int(input("Guess the winning number:"))
-print(f"The lucky number is {win_number}")
-if int(guess) == win_number:
-    print("Wow! Such a genius!")
-else:
-    print("You suck at this!")
+
+play_again = "y"
+while play_again == "y":
+    # generate a random number
+    win_number = random.randint(1, 3)
+    guess = int(input("Guess the winning number:"))
+    print(f"The lucky number is {win_number}")
+    if int(guess) == win_number:
+        print("Wow! Such a genius!")
+    else:
+        print("You suck at this!")
+    play_again = input("Do you dare to play again? (y/n)")
+
