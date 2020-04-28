cef8de8f30d08d74e65dd13571d4c3d4da0c3a92
lesley <lesleyjoseph024@gmail.com>
2020-04-27 20:45:28 -0400

    betting game v1.7 Input Validation testing completed not operator introduced, returns the opposite value of a boolean variable or value. isnumeric() = this is to make the input a numeric value guess.isnumeric() = the dot notation is for python to read guess as a variable instead of a string attached to the isnumeric() num_guess is a new variable to capture the int conversion of guess in operator determines if a value is within a list or collenction and retuns true if it is. The range() function creates a list of values that begins from the first parameter and ending 1 short of the last parameter

--- betting.py
+++ betting.py
@@ -1,28 +1,35 @@
 import random
 
-valid = False
-while not valid:
-    guess = input(f"Guess the lucky number (between 1 and {limit}):")
-#adding upper limit
+# adding upper limit
 limit = 5
-#score tracking variables
+# score tracking variables
 wins = losses = 0
+# main game loop
 play_again = "y"
 while play_again == "y":
     # generate a random number
     win_number = random.randint(1, limit)
-    guess = int(input(f"Guess the lucky number (between 1 and {limit}):"))
-    print()
-    print(f"The lucky number is {win_number}")
-    if int(guess) == win_number:
+    #input validation
+    valid = False
+    while not valid:
+        guess = input(f"Guess the lucky number (between 1 and {limit}):")
+        print() #this function is to skip a line after input
+        if guess.isnumeric():
+            num_guess = int(guess)
+        # we can never get a value error here because we know guess is numeric
+            if num_guess in range(1, limit + 1):
+                valid = True
+
+    if num_guess == win_number:
+        print(f"The lucky number is {win_number}")
         wins += 1
         print("Wow! Such a genius!")
     else:
         losses += 1
         print("You suck at this!")
-        print()
-    print(f"Score:wins{wins}, losses{losses}")
+    print()
+    print(f"Score:wins{ wins}, losses{ losses}")
     print()
     play_again = input("Do you dare to play again? (y/n)")
-    print()
+
 
