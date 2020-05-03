ad9780c3a8e182404af1c828e25e6503afde1779
lesley <lesleyjoseph024@gmail.com>
2020-05-02 20:42:25 -0400

    betting game v1.8 Here we introduced a scoring system that also gave hints to the user to guess the correct answer. They were given 3 chances to guess correctly and if they didn't then they lost the game. To do this elif function was introduced. elif allows more than 1 option for an if/else function. With elif, it tells the program that if something happens, do this, else if another condition happens, do that, else if something else happens do a particular function. elif allows you to add more than 2 options to a situation unlike an if/else where only 2 options are available. so, with elif, if the first condition isn't true, we can check a second or third condition until it is met. For each chance that the player is given an input validation was completed so that there would be no errors and the game would not crash. another new function that was introduced is the != operator. This checks if 2 values are not equal and returns as True, and if they aren't. If the value of num_guess is already the same as win_number, then the user guessed correctly so the != condition is false and all the code that depends on that condition is bypassed.

--- betting.py
+++ betting.py
@@ -1,9 +1,10 @@
 import random
 
 # adding upper limit
-limit = 5
+limit =10
 # score tracking variables
-wins = losses = 0
+#wins = losses = 0
+score = 0
 # main game loop
 play_again = "y"
 while play_again == "y":
@@ -22,14 +23,65 @@ while play_again == "y":
 
     if num_guess == win_number:
         print(f"The lucky number is {win_number}")
-        wins += 1
-        print("Wow! Such a genius!")
+        score += 5
+        print()
+        print("Awesome! You nailed it on the first try!")
+    elif num_guess > win_number:
+        print ()
+        print("Aww, poor looser! Try guessing lower this time.")
+        print()
     else:
-        losses += 1
-        print("You suck at this!")
+        print ()
+        print("What are you stupid? Guess higher this time.")
+        print()
+
+    if num_guess != win_number:
+        valid = False
+        while not valid:
+            guess = input (f"Guess the lucky number (between 1 and {limit}):")
+            print()
+            if guess.isnumeric ():
+                num_guess = int(guess)
+                # we can never get a value error here because we know guess is numeric
+                if num_guess in range (1, limit +1):
+                    valid = True
+        if num_guess == win_number:
+            score += 3
+            print()
+            print("Hooray! You guessed correctly! I'm proud of you!")
+            print()
+        elif num_guess > win_number:
+            print()
+            print("You're not that smart are you? Why don't you guess lower this time?")
+            print()
+        else:
+            print("I really thought you would get it right this round! Guess higher.")
+            print()
+
+    if num_guess != win_number:
+        valid = False
+        while not valid:
+            guess = input (f"Guess the lucky number (between 1 and {limit}):")
+            print()
+            if guess.isnumeric ():
+                num_guess = int(guess)
+                # we can never get a value error here because we know is numeric
+                if num_guess in range (1, limit +1):
+                    valid = True
+        if num_guess == win_number:
+            print()
+            score += 1
+            print("Finally! You guessed the correct number!")
+            print()
+        else:
+            print (f"You're a failure that can't guess a simple number! The winning number is {win_number}")
+            print()
+
+    print(f"Score:{ score}")
     print()
-    print(f"Score:wins{ wins}, losses{ losses}")
-    print()
+
     play_again = input("Do you dare to play again? (y/n)")
+    print()
+
 
 
