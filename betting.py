52cdea4af729018fc2d20fc0b6920609e42a8f58
lesley <lesleyjoseph024@gmail.com>
2020-05-03 20:24:37 -0400

    betting.py v9 Creating a function so that there is no need to repeat the code block everywhere in the code to avoid repetition of the codes. Makes it simpler especially for input validation. Def = define the function that you area creating to be inputted into the code. Make sure to name the function after what it does so that you will understand and it will make sense to you!

--- betting.py
+++ betting.py
@@ -1,50 +1,42 @@
 import random
 
 # adding upper limit
-limit =10
+limit = 10
 # score tracking variables
-#wins = losses = 0
-score = 0
-# main game loop
-play_again = "y"
-while play_again == "y":
-    # generate a random number
-    win_number = random.randint(1, limit)
-    #input validation
+# wins = losses = 0
+score: int = 0
+
+# This is creating our own function to test input validation so that we don't have to be repetitive.
+def get_valid_number():
     valid = False
     while not valid:
         guess = input(f"Guess the lucky number (between 1 and {limit}):")
-        print() #this function is to skip a line after input
-        if guess.isnumeric():
-            num_guess = int(guess)
-        # we can never get a value error here because we know guess is numeric
-            if num_guess in range(1, limit + 1):
-                valid = True
+        if guess.isnumeric() and int(guess) in range(1, limit + 1):
+            valid = True
+            return int(guess)
 
+# main game loop
+play_again = "y"
+while play_again == "y":
+    # User's first chance:
+    win_number = random.randint(1, limit)
+    num_guess = get_valid_number()
     if num_guess == win_number:
         print(f"The lucky number is {win_number}")
-        score += 5
         print()
         print("Awesome! You nailed it on the first try!")
+        score += 5
     elif num_guess > win_number:
-        print ()
+        print()
         print("Aww, poor looser! Try guessing lower this time.")
         print()
     else:
-        print ()
+        print()
         print("What are you stupid? Guess higher this time.")
         print()
-
+    # User's second chance:
     if num_guess != win_number:
-        valid = False
-        while not valid:
-            guess = input (f"Guess the lucky number (between 1 and {limit}):")
-            print()
-            if guess.isnumeric ():
-                num_guess = int(guess)
-                # we can never get a value error here because we know guess is numeric
-                if num_guess in range (1, limit +1):
-                    valid = True
+        num_guess = get_valid_number()
         if num_guess == win_number:
             score += 3
             print()
@@ -57,31 +49,20 @@ while play_again == "y":
         else:
             print("I really thought you would get it right this round! Guess higher.")
             print()
-
+    # User's final chance:
     if num_guess != win_number:
-        valid = False
-        while not valid:
-            guess = input (f"Guess the lucky number (between 1 and {limit}):")
-            print()
-            if guess.isnumeric ():
-                num_guess = int(guess)
-                # we can never get a value error here because we know is numeric
-                if num_guess in range (1, limit +1):
-                    valid = True
+        num_guess = get_valid_number()
         if num_guess == win_number:
             print()
             score += 1
             print("Finally! You guessed the correct number!")
             print()
         else:
-            print (f"You're a failure that can't guess a simple number! The winning number is {win_number}")
+            print(f"You're a failure that can't guess a simple number! The winning number is {win_number}")
             print()
 
-    print(f"Score:{ score}")
+    print(f"Score:{score}")
     print()
 
     play_again = input("Do you dare to play again? (y/n)")
     print()
-
-
-
